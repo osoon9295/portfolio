22a020e26347924dcaaf41f25971794312c5c421
Boyoung Lee <osoon9295@gmail.com>
2025-08-06 10:28:06 +0900

    [BOPO-3] BOPO-7 키워드 생성

--- src/components/about/Keyword.tsx
+++ src/components/about/Keyword.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Keyword = () => {
-  return <div>keyword</div>;
+  return (
+    <div className="w-full h-screen flex flex-row items-center justify-center gap-4">
+      <div className="bg-red-100 w-fit">도전 정신</div>
+      <div className="bg-yellow-100 w-fit">구현의 즐거움</div>
+      <div className="bg-green-200 w-fit">협업</div>
+    </div>
+  );
 };
 
 export default Keyword;
