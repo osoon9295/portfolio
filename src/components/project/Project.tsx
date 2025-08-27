f913fccc337c4f082ee18edcb3068147e1b4d675
Boyoung Lee <osoon9295@gmail.com>
2025-08-27 20:24:05 +0900

    [BOPO-14] project, contact 반응형 추가

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -9,7 +9,7 @@ const Project = ({ project }: { project: ProjectProps }) => {
   return (
     <figure className="flex flex-col gap-4 border-1 w-[300px] h-[460px] p-4 rounded-3xl hover:border-lime-600 hover:text-lime-600">
       <div className="flex flex-row justify-between">
-        <h3 className="font-[800]">{project.name}</h3>
+        <h3 className="font-[800] text-xl">{project.name}</h3>
         <Link href={project.github}>
           <FaGithub size={25} />
         </Link>
