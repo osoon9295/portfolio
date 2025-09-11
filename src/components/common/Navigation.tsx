aeae9ebe1fbc444d8eed5affb8115f154a161a15
Boyoung Lee <osoon9295@gmail.com>
2025-09-11 23:12:43 +0700

    [BOPO-26] 네비게이션 스크롤이벤트 추가

--- src/components/common/Navigation.tsx
+++ src/components/common/Navigation.tsx
@@ -1,21 +1,41 @@
+"use client";
 import React from "react";
 import { GiJupiter } from "react-icons/gi";
 import { IoPlanetOutline, IoPlanetSharp } from "react-icons/io5";
 import { RiSpaceShipFill } from "react-icons/ri";
 
 const Navigation = () => {
+  const handleScroll = (page: number) => {
+    window.scrollTo({
+      top: window.innerHeight * page,
+      behavior: "smooth",
+    });
+  };
+
   return (
-    <div className="relative w-full h-screen">
-      <div className="fixed left-[20%] top-[20%] text-4xl">
+    <div>
+      <div
+        className="fixed left-[25%] top-[25%] text-4xl cursor-pointer"
+        onClick={() => handleScroll(1)}
+      >
         <IoPlanetOutline />
       </div>
-      <div className="fixed left-[50%] top-[70%] text-4xl">
+      <div
+        className="fixed left-[50%] top-[70%] text-4xl"
+        onClick={() => handleScroll(2)}
+      >
         <IoPlanetSharp />
       </div>
-      <div className="fixed left-[80%] top-[50%] text-4xl">
+      <div
+        className="fixed left-[80%] top-[50%] text-4xl"
+        onClick={() => handleScroll(3)}
+      >
         <GiJupiter />
       </div>
-      <div className="fixed left-[60%] top-[30%] text-4xl">
+      <div
+        className="fixed left-[60%] top-[30%] text-4xl"
+        onClick={() => handleScroll(4)}
+      >
         <RiSpaceShipFill />
       </div>
     </div>
